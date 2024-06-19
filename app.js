1b6e2a8503ff06a9592e87e7c35c3b2100f23fd7
PushpinderSekhon <pushpindersekhon0@gmail.com>
2024-06-19 14:11:25 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls')
+const sectBtn = document.querySelectorAll('.control')
+const allSections = document.querySelector('.main-content')
+
+function pageSwitch()//switches pages based on clicked button
+{
+    for (let i =0; i<sectBtn.length;i++)
+    {
+        sectBtn[i].addEventListener('click', function() 
+        {
+            let currentButton = document.querySelectorAll('.active-btn')
+            currentButton[0].className = currentButton[0].className.replace('active-btn','')
+            this.className += ' active-btn'
+
+        })
+    }
+    //
+    allSections.addEventListener('click', (e)=>{
+        const id = e.target.dataset.id;
+        if(id)
+        {
+            sectBtns.forEach((btn)=>
+            {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            sections.forEach((section)=>
+            {
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id)
+            element.classList.add('active')
+        }
+    })
+    //allSections.foreach((section) =>)
+}
+pageSwitch()
+
